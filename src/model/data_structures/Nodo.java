9e9acc7ebf4dd553741409840dfc8b5bb5e2cb18
Gustavo-Tabima <=>
2020-02-14 16:31:39 -0500

    añadidas clase nodo y comparendo

---
+++ src/model/data_structures/Nodo.java
@@ -0,0 +1,34 @@
+package model.data_structures;
+
+public class Nodo <K>  {
+
+	private Nodo <K> siguiente;
+	
+
+	private K datos;
+
+	public Nodo(K pDatos) {
+		datos = pDatos;
+	}
+	public K darComparendo() {
+		return datos;
+	}
+
+	public Nodo darSiguente() {
+		return siguiente;
+	}
+	
+	
+	public void setSiguiente(Nodo psiguiente) {
+		
+		siguiente=psiguiente;
+		
+	}
+
+	
+	
+	public void setDato(K dato) 
+	{
+		datos = dato;
+	}
+}
